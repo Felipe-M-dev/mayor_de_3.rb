a2344b51ca574d4bd6070968298c43c218d7b5f9
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2021-06-16 20:42:17 -0400

    Create mayor_que_3.rb
    Add script

---
+++ mayor_que_3.rb
@@ -0,0 +1,22 @@
+#Inicio
+#Ingresar 3 números de cualquier valor
+#Si n_1 es mayor o igual que n_2 y n_2 muestra n_1
+#Si n_2 es mayor o igual que n_3 muestra n_2
+#Sino muestra n3
+#Fin
+
+## Args
+
+n_1 = ARGV[0].to_i
+n_2 = ARGV[1].to_i
+n_3 = ARGV[2].to_i
+
+## Main
+
+if n_1 >= n_2 && n_1 >= n_3
+    puts n_1 
+elsif n_2 >= n_3
+    puts n_2
+else
+    puts n_3
+end
