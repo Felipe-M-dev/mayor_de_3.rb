2bd0c3ab260121bbb0066e4e7a7741cb700b9c8f
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2021-06-17 14:31:18 -0400

    Update mayor_que_3.rb

--- mayor_que_3.rb
+++ mayor_que_3.rb
@@ -1,6 +1,6 @@
 #Inicio
 #Ingresar 3 números de cualquier valor
-#Valida que sean 3 parámetros los ingresados
+#Valida que sean 3 parámetros los ingresados con nil
 #Si n_1 es mayor o igual que n_2 y n_2 muestra n_1
 #Si n_2 es mayor o igual que n_3 muestra n_2
 #Sino muestra n_3
@@ -8,20 +8,20 @@
 
 ## Args
 
-n_1 = ARGV[0].to_i
-n_2 = ARGV[1].to_i
-n_3 = ARGV[2].to_i
+n_1 = ARGV[0]
+n_2 = ARGV[1]
+n_3 = ARGV[2]
 
 ## Main
 
-if !n_3.nil?
+if n_3.nil?
     puts "Ingrese 3 parámetros"
 else
-    if n_1 >= n_2 && n_1 >= n_3
+    if n_1.to_i >= n_2.to_i  && n_1.to_i  >= n_3.to_i 
         puts n_1
-    elsif n_2 >= n_3
-        puts n_2
+    elsif n_2.to_i  >= n_3.to_i 
+        puts n_2.to_i 
     else
-        puts n_3
+        puts n_3.to_i 
     end
 end
